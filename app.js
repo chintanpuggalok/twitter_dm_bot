bb9b9d78570833839549261c6bb77b15941ed7b7
chintanpuggalok <55681139+chintan29012001@users.noreply.github.com>
2023-01-21 03:08:07 +0530

    added encryption module

--- app.js
+++ app.js
@@ -5,6 +5,9 @@
 import * as dotenv from "dotenv";
 import express from "express";
 import { TwitterApi } from 'twitter-api-v2';
+import url_unshort from 'url-unshort';
+// const {uu}=url_unshort;
+import crypto from 'crypto';
 
 // Instantiate with desired auth type (here's Bearer v2 auth)
 // let req;
@@ -51,30 +54,50 @@ app.get('/callback', async (req, res) => {
       
       const client = new TwitterApi({ clientId: process.env.CLIENT_ID, clientSecret: process.env.CLIENT_SECRET });
       const { client: loggedClient, accessToken, refreshToken, expiresIn } = await client.loginWithOAuth2({ code, codeVerifier, redirectUri: CALLBACK_URL })
-        const tweets = await loggedClient.v2.search('conversation_id:1612850822202941440',{expansions:['author_id']
-      })
+      const eventTimeline = await loggedClient.v2.listDmEvents()
+      // console.log(eventTimeline.events[0].text)
+      const uu=url_unshort();
+      const url=await uu.expand(eventTimeline.events[1].text)
+      // console.log(url.findIndex('/status/'))
+      let index=url.lastIndexOf('/')+1
+      let id=url.slice(index)
+      console.log(id)
+      loggedClient.v2.tweet
+      // if (url) console.log('Original url is: ${url}')
+      // else console.log('This url can\'t be expanded')
+      // let response=https.get(eventTimeline.events[0].text);
+      // https.get(eventTimeline.events[0].text,(response)=>{
+      //   console.log(response.url)
+      //   res.send(response.url)
 
-      let users=tweets['includes']?tweets['includes']['users']:[];
-      // console.log(users);
-      // let commentors=new Set();
-      users?.forEach(async (user)=>{
-        console.log(user);
-        try {
-          const { dm_conversation_id, dm_event_id } = await loggedClient.v2.sendDmToParticipant( user.id,{
-            text:'sent by the bot'
-          }
-          )
-          console.log(dm_conversation_id)
+      // });
+      // uu.
+
+
+      //   const tweets = await loggedClient.v2.search('conversation_id:1612850822202941440',{expansions:['author_id']
+      // })
+
+      // let users=tweets['includes']?tweets['includes']['users']:[];
+      // // console.log(users);
+      // // let commentors=new Set();
+      // users?.forEach(async (user)=>{
+      //   console.log(user);
+      //   try {
+      //     const { dm_conversation_id, dm_event_id } = await loggedClient.v2.sendDmToParticipant( user.id,{
+      //       text:'sent by the bot'
+      //     }
+      //     )
+      //     console.log(dm_conversation_id)
           
-        } catch (error) {
-          console.log(error)
-        }
+      //   } catch (error) {
+      //     console.log(error)
+      //   }
         
-        // loggedClient.v2.  
+      //   // loggedClient.v2.  
 
 
-      })
-
+      // })
+      res.send({'message':"ended normal"})
         
     } catch (error) {
       console.log(error)
